033ad1368b1412f6f700a7905810029fb2a9af1f
Topher <tophersymps@gmail.com>
2018-07-14 01:40:18 -0400

    Started a new brew

---
+++ portfolio/src/components/Header/HeaderLinks.jsx
@@ -0,0 +1,121 @@
+/*eslint-disable*/
+import React from "react";
+// react components for routing our app without refresh
+import { Link } from "react-router-dom";
+
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Tooltip from "@material-ui/core/Tooltip";
+
+// @material-ui/icons
+import { Apps, CloudDownload } from "@material-ui/icons";
+
+// core components
+import CustomDropdown from "components/CustomDropdown/CustomDropdown.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+
+import headerLinksStyle from "assets/jss/material-kit-react/components/headerLinksStyle.jsx";
+
+function HeaderLinks({ ...props }) {
+  const { classes } = props;
+  return (
+    <List className={classes.list}>
+      {/* <ListItem className={classes.listItem}>
+        <CustomDropdown
+          noLiPadding
+          buttonText="Components"
+          buttonProps={{
+            className: classes.navLink,
+            color: "transparent"
+          }}
+          buttonIcon={Apps}
+          dropdownList={[
+            <Link to="/" className={classes.dropdownLink}>
+              All components
+            </Link>,
+            <a
+              href="https://creativetimofficial.github.io/material-kit-react/#/documentation"
+              target="_blank"
+              className={classes.dropdownLink}
+            >
+              Documentation
+            </a>
+          ]}
+        />
+      </ListItem> */}
+      
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="instagram-twitter"
+          title="Follow me on Twitter"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            href="https://twitter.com/tophersymps"
+            target="_blank"
+            color="transparent"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fab fa-twitter"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="instagram-tooltip"
+          title="Follow me on Instagram"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            color="transparent"
+            href="https://www.instagram.com/tophersymps"
+            target="_blank"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fab fa-instagram"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="instagram-linkedin"
+          title="Follow me on LinkedIn"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            color="transparent"
+            href="https://www.linkedin.com/in/csympson"
+            target="_blank"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fab fa-linkedin"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="instagram-github"
+          title="Check out my Repos!"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            color="transparent"
+            href="https://www.github.com/tophersymps"
+            target="_blank"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fab fa-github"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+    </List>
+  );
+}
+
+export default withStyles(headerLinksStyle)(HeaderLinks);
---
+++ portfolio/src/views/LandingPage/LandingPage.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+
+// @material-ui/icons
+
+// core components
+import Header from "components/Header/Header.jsx";
+import Footer from "components/Footer/Footer.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+// UNUSED >> import Button from "components/CustomButtons/Button.jsx";
+import HeaderLinks from "components/Header/HeaderLinks.jsx";
+import Parallax from "components/Parallax/Parallax.jsx";
+
+import landingPageStyle from "assets/jss/material-kit-react/views/landingPage.jsx";
+
+// Sections for this page
+import ProductSection from "./Sections/ProductSection.jsx";
+import TeamSection from "./Sections/TeamSection.jsx";
+import WorkSection from "./Sections/WorkSection.jsx";
+import profile from "assets/img/personal/thisisus.jpg";
+
+const dashboardRoutes = [];
+
+class LandingPage extends React.Component {
+  render() {
+    const { classes, ...rest } = this.props;
+    return (
+      <div>
+        <Header
+          color="transparent"
+          routes={dashboardRoutes}
+          brand="Christopher Sympson"
+          rightLinks={<HeaderLinks />}
+          fixed
+          changeColorOnScroll={{
+            height: 400,
+            color: "white"
+          }}
+          {...rest}
+        />
+        <Parallax filter image={require("assets/img/personal/NYC1.jpg")}>
+          <div className={classes.container}>
+            <GridContainer>
+              <GridItem xs={12} sm={12} md={6} className={classes.center}>
+                <img
+                  src={profile}
+                  alt="..."
+                  className={
+                    classes.imgRaised +
+                    " " +
+                    classes.imgRoundedCircle +
+                    " " +
+                    classes.imgFluid
+                  }
+                />
+              </GridItem>
+              <GridItem xs={12} sm={12} md={6}>
+                <h1 className={classes.title}>Hello World!</h1>
+                <h4>
+                  Web Developer | Photographer | Business Consultant
+                </h4>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </Parallax>
+        <div className={classNames(classes.main, classes.mainRaised)}>
+          <div className={classes.container}>
+            {/* <ProductSection /> */}
+            {/* <TeamSection /> */}
+            {/* <WorkSection /> */}
+          </div>
+        </div>
+        {/* <Footer /> */}
+      </div>
+    );
+  }
+}
+
+export default withStyles(landingPageStyle)(LandingPage);
